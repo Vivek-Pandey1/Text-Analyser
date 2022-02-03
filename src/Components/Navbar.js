4eae23a78b31cf4cc2c16418a74f92e14e9ccf77
Vivek Pandey <keviv2698@gmail.com>
2022-02-03 20:30:18 +0530

    feature: can remove extra space

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {Button,Tooltip,OverlayTrigger} from 'react-bootstrap';
+import { Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 
-const Navbar = (props) => {
+const Navbar = () => {
   const [theemMode, setTheemMode] = useState("Enable dark mode");
   const mode = () => {
     var element = document.body;
     var textInput = document.getElementById("myText");
     element.classList.toggle("dark");
     textInput.classList.toggle("dark");
+
     if (theemMode === "Enable dark mode") {
       setTheemMode("Enable light mode");
-       props.navbarmode = "dark";
     } else if (theemMode === "Enable light mode") {
       setTheemMode("Enable dark mode");
-      props.navbarmode = "light";
     }
   };
-  /////////
+
   const renderTooltip = (props) => (
     <Tooltip id="button-tooltip" {...props}>
       {theemMode}
     </Tooltip>
   );
-  
- 
-  
 
-  //////////
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-{navbarmode} bg-{navbarmode}`}
-    >
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid ">
         <Link className="navbar-brand" to="/">
           <img src="./logo.svg" alt="logo" />
         </Link>
@@ -43,25 +36,25 @@ const Navbar = (props) => {
         <Link className="nav-link nav-item" to="/about">
           About
         </Link>
+       
       </div>
-
       <OverlayTrigger
-      placement="bottom"
-      delay={{ show: 250, hide: 400 }}
-      overlay={renderTooltip}
-    >
-      <Button variant="checkbox" onClick={mode}>
-      
-        <div className="form-check form-switch ">
-        <input
-            className="form-check-input btn-lg"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckDefault"
+        placement="bottom"
+        delay={{ show: 250, hide: 400 }}
+        overlay={renderTooltip}
+      >
+        <Button variant="checkbox" onClick={mode}>
+          <div className="form-check form-switch ">
+            <input
+              className="form-check-input btn-lg"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
             />
-            </div>
+          </div>
         </Button>
-    </OverlayTrigger>
+      </OverlayTrigger>
+     
     </nav>
   );
 };
