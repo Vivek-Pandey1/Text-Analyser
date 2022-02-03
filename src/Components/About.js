34a338735d6f148f438d9fc0245de3b3c848aae6
Vivek Pandey <keviv2698@gmail.com>
2022-02-03 20:34:04 +0530

    feature: remove extra space

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-  return <div><h1>hi im about me</h1></div>;
+  return (
+    <div className="container">
+      <h1>About text analyzer</h1>
+      <ul className="list-group list-group-flush text-start fs-4">
+        <li className="list-item m-3">
+          With the help of text analyzer you can alter your text to capital-case, small-case or to Title-case
+        </li>
+        <li className="list-item m-3">
+        You can remove the excess space between the text
+        </li>
+        <li className="list-item m-3">
+          Also you can copy and clear the text 
+        </li>
+        
+        
+      </ul>
+    </div>
+  );
 };
 
 export default About;
