acf6c3fb098fd03c41bbd15b86ecd17a5399adf4
Vivek Pandey <keviv2698@gmail.com>
2022-02-04 18:27:35 +0530

    Chore: updated not found

--- src/Components/Notfound.js
+++ src/Components/Notfound.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Notfound = () => {
   return (
     <div>
-      <h1 className="text-primary m-3">Oops!</h1>
-      <h1>Error: 404 page not found <br /> You weren't supposed to see this</h1>
-      <p>The page you're looking for no longer exists <br/>
+      <h1 className="text-primary m-3">Oops!</h1> <br/>
+      <h2>Error: 404 page not found <br /> You weren't supposed to see this</h2> <br/>
+      <p>The page you're looking for no longer exists <br/> 
        Return to <Link to="/">home page</Link></p>
     </div>
   );
